bb2c6981b1298c0cd13ab49f191d9885c4aa5630
Andrew Hutton <fromalaska49@gmail.com>
2017-01-17 16:16:31 -0600

    Uploading Stuff

---
+++ photos.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+			require('common/head-includes.php');
+		?>
+		<title>
+			Paul Greenlee Retirement
+		</title>
+	</head>
+	<body>
+		<?php
+			require('common/menu.php');
+		?>
+		<div id="content" class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="row section-container">
+						<h2 class="col-sm-12">
+							Retirement Ceremony Video
+						</h2>
+					</div>
+					<div class="row youtube-embed-container">
+						<div class="col-sm-12">
+							<iframe class="responsive-youtube-embed" src="https://www.youtube.com/embed/9Duuf2kqkl4?rel=0" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<div class="row section-container">
+						<div class="col-sm-12">
+							On Friday, January 13, 2017, Lt Col Paul Greenlee celebrated his retirement from the Air Force, with family and friends, after 24 years of dedication and service to the nation.
+						</div>
+					</div>
+					<!--
+						<div class="row section-container" style="display:table;" id="slack-container">
+							<div class="col-sm-8">
+								<h1>
+									Slack
+								</h1>
+								<p>
+									Want to share notes from class? Want to hear about events on campus or talk about the latest and greatest stuff in technology? From app development to unix security, ACM has people who share your interests. The best way to get connected with other students and hear about upcoming events, is through <a href="http://acm-utsa.slack.com">Slack</a>. We use Slack to share information about internships, events, and opportunities at UTSA and around town. Members can recieve an invitation to join by sending an email to <a href="mailto:acm.utsa@gmail.com">acm.utsa@gmail.com</a> or by contacting one of our <a href="officers.php">officers</a>.
+								</p>
+							</div>
+							<div class="col-sm-4 hidden-xs">
+								<div style="margin:120px 0px 0px 0px;text-align: center;">
+									<a href="http://acm-utsa.slack.com">
+										<input type="image" src="images/icons/add-to-slack.png" class="slack-button"/>
+									</a>
+								</div>
+							</div>
+							<div class="col-sm-4 hidden-sm hidden-md hidden-lg hidden-xl">
+								<div style="margin:40px 0px 0px 0px;text-align: center;">
+									<a href="http://acm-utsa.slack.com">
+										<input type="image" src="images/icons/add-to-slack.png" class="slack-button"/>
+									</a>
+								</div>
+							</div>
+						</div>
+					-->
+				</div>
+			</div>
+		</div>
+		<?php
+			require('common/footer.php');
+		?>
+	</body>
+</html>
